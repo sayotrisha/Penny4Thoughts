91f08c5fe900091a2b14c2c1d0360fd53ebebc88
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2024-10-19 07:39:46 +0800

    initialize

---
+++ MCO/app/src/main/java/com/mobdeve/s12/group4/mco/Act2SignUp.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s12.group4.mco
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Act2SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_signup)
+    }
+}
