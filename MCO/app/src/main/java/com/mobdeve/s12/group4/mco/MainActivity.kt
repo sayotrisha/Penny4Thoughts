ffbc60ae804f4c6dda5454041d9fa03ae664f6f3
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2024-10-19 18:50:43 +0800

    feat: add navdrawer

--- MCO/app/src/main/java/com/mobdeve/s12/group4/mco/MainActivity.kt
+++ MCO/app/src/main/java/com/mobdeve/s12/group4/mco/MainActivity.kt
@@ -1,20 +1,54 @@
 package com.mobdeve.s12.group4.mco
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView : BottomNavigationView
+    private lateinit var toggle : ActionBarDrawerToggle
+    private lateinit var drawerLayout : DrawerLayout
+    private lateinit var toolbar : Toolbar
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+
+        val homeFragment = HomeFragment()
+        val recordsFragment = RecordsFragment()
+
+        setCurrentFragment(homeFragment)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> setCurrentFragment(homeFragment)
+                R.id.record -> setCurrentFragment(recordsFragment)
+            }
+            true
+        }
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
         }
     }
 }
