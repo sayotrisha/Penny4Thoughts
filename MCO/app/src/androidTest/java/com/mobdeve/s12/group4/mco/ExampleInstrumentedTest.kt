76c91acb57c2e591ca3798e24bb7923989b93bcf
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2024-10-19 08:27:43 +0800

    initialize new project

--- MCO/app/src/androidTest/java/com/mobdeve/s12/group4/mco/ExampleInstrumentedTest.kt
+++ MCO/app/src/androidTest/java/com/mobdeve/s12/group4/mco/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.mobdeve.s12.rebenque.paula.mco", appContext.packageName)
+        assertEquals("com.mobdeve.s12.group4.mco", appContext.packageName)
     }
 }
--- MCO/app/src/main/java/com/mobdeve/s12/group4/mco/MainActivity.kt
+++ MCO/app/src/main/java/com/mobdeve/s12/group4/mco/MainActivity.kt
@@ -1,26 +1,20 @@
 package com.mobdeve.s12.group4.mco
 
-import android.content.Intent
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.act_welcome)
-
-        val btnLogin: Button = findViewById(R.id.btnLogin)
-        val btnSignup: Button = findViewById(R.id.btnSignup)
-
-        btnLogin.setOnClickListener {
-            val intent = Intent(this, Act3LogIn::class.java)
-            startActivity(intent)
-        }
-
-        btnSignup.setOnClickListener {
-            val intent = Intent(this, Act2SignUp::class.java)
-            startActivity(intent)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
