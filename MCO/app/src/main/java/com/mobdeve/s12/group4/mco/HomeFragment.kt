024e8a3d48c193fa1284f4f4230350177f0a40ae
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2024-10-25 04:16:26 +0800

    feat: add home fragment and popup layouts

--- MCO/app/src/main/java/com/mobdeve/s12/group4/mco/HomeFragment.kt
+++ MCO/app/src/main/java/com/mobdeve/s12/group4/mco/HomeFragment.kt
@@ -1,7 +1,26 @@
 package com.mobdeve.s12.group4.mco
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeFragment : Fragment(R.layout.home_fragment){
+    private lateinit var accountsRV: RecyclerView
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        accountsRV = view.findViewById(R.id.accountsRV)
+
+        val accounts = DataGenerator.generateAccountData()
+        val accountsAdapter = AccountsAdapter(accounts)
+
+        accountsRV.adapter = accountsAdapter
+        accountsRV.layoutManager = LinearLayoutManager(requireContext())
+
+
+
+    }
 }
